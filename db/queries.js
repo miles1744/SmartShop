ff1d103c19fbbef616d432396a3c1852e1e7b517
Miles1744 <mithomtab@gmail.com>
2025-05-06 14:37:50 -0400

    New

--- db/queries.js
+++ db/queries.js
@@ -16,16 +16,21 @@ async function getAllGroceries(searchTerm) {
 }
 
 async function insertGroceries(item) {
-    await pool.query("INSERT INTO groceries (item) VALUES ($1)", [username]);
+    await pool.query("INSERT INTO groceries (item) VALUES ($1)", [item]);
 }
 
 async function deleteUsers() {
-    await pool.query("DELETE FROM usernames")
+    await pool.query("DELETE FROM groceries")
 }
 
+async function getMessageById(id) {
+    const result = await pool.query("SELECT * FROM groceries WHERE id = $1", [id]);
+    return result.rows
+}
 
 module.exports = {
     getAllGroceries,
     insertGroceries,
-    deleteUsers
+    deleteUsers,
+    getMessageById
 };
