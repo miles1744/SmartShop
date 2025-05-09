32215175fee71f75ae7b6ac2939df9acd66e7121
Miles1744 <mithomtab@gmail.com>
2025-05-09 19:42:54 -0400

    New

--- client/src/UpdateGrocery.jsx
+++ client/src/UpdateGrocery.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 
 const UpdateGrocery = () => {
@@ -74,6 +74,10 @@ const UpdateGrocery = () => {
             </select>
             <button type="submit">Update Grocery</button>
         </form>
+
+        <Link to="/">
+        <button className="home-button">Home</button>
+      </Link>
     </div>
   );
 };
