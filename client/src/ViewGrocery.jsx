956d78208b799a26bbd44e84cd62ccbbc8e7d684
Miles1744 <mithomtab@gmail.com>
2025-05-09 22:00:49 -0400

    New

--- client/src/ViewGrocery.jsx
+++ client/src/ViewGrocery.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 const ViewGrocery = () => {
   const { id } = useParams();
   const [grocery, setGrocery] = useState(null);
+  const [category, setCategory] = useState([]);
 
   useEffect(() => {
     const fetchGrocery = async () => {
@@ -20,6 +21,21 @@ const ViewGrocery = () => {
     fetchGrocery();
   }, [id]);
 
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const res = await axios.get(`http://localhost:3000/categories`);
+        console.log("Fetched category:", res.data);
+        setCategory(res.data);
+      } catch (err) {
+        console.error("Failed to fetch category:", err);
+      }
+    };
+
+    fetchCategories();
+  }, [id]);
+
   if (!grocery) {
     return 
   }
@@ -30,7 +46,7 @@ const ViewGrocery = () => {
         <h1>Item: {grocery.item}</h1>
         <p>Price: ${grocery.price}</p>
         <p>Quantity: {grocery.quantity}</p>
-
+        <p>Category: {grocery.categeroyId}</p>
       </div>
 
       <Link to="/">
