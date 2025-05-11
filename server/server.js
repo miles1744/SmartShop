ea7781a8df576d98f88a0d8b608385007c4754c4
Miles1744 <mithomtab@gmail.com>
2025-05-10 20:56:00 -0400

    New

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const groceryRoutes = require("./routes/groceryroutes.js");
 const cors = require("cors");
 
 const corsOptions = {
-    origin:["http://localhost:5173"],
+    origin:["http://localhost:5173", "https://smartshop-frontend-382d.onrender.com"],
 };
 
 app.use(express.json());
