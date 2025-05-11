38f24b7c2db4d19ecaa877d1855a28bb137b148e
Miles1744 <mithomtab@gmail.com>
2025-05-10 21:37:35 -0400

    New

--- client/src/CategoriesList.jsx
+++ client/src/CategoriesList.jsx
@@ -29,7 +29,7 @@ const CategoriesList = () => {
 
       const handleDelete = async (categoryId) => {
         try {
-          await axios.post(`${API_BASE}/categories`, {
+          await axios.post(`${API_BASE}/categories${categoryId}`, {
             id: categoryId,
           });
           setResults(prev => prev.filter(cat => cat.id !== categoryId));
