8d359415f4cbc5e33c93727747ac4ef9f5e36e9b
Miles1744 <mithomtab@gmail.com>
2025-05-10 21:45:43 -0400

    New

--- server/db/init_db.js
+++ server/db/init_db.js
@@ -38,12 +38,12 @@ async function main() {
     console.log("Seeding...");
 
     const client = new Client({
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        database: process.env.DB_NAME
-    });
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false, // Required for Render-hosted databases
+        },
+      });
+      
 
     await client.connect();
     try {
